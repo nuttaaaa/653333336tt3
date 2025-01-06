a23ec79b409b2ade3cdfd5f6b68fcdf58466dcf4
nuttaaaa <122249892+nuttaaaa@users.noreply.github.com>
2025-01-05 17:27:11 -0800

    Update help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -136,13 +136,12 @@ module.exports = {
                         case 'anime':
                             page.image = "https://cdn.discordapp.com/attachments/1246408947708072027/1253714802048499752/1111.gif?ex=6676dc65&is=66758ae5&hm=9bc3f45ed4930d62def2369c6a27fdd65f24df0fdbe557a7ff7d330090eac1bf&";
                             page.color = "#ff66cc";
-                            page.thumbnail = "https://cdn.discordapp.com/attachments/1264134884432285766/1270353776489922651/s_removed.png?ex=66b364a6&is=66b21326&hm=2d4ded259b57f476c901c58401e1d6faba2653c0a964ee1c4f371ecf3da52100&";
-                            page.author.iconURL = "https://cdn.discordapp.com/attachments/1246408947708072027/1255167194036437093/1558-zerotwo-exciteddance.gif?ex=667c250a&is=667ad38a&hm=09e6db36fd79436eb57de466589f21ca947329edd69b8e591d0f6586b89df296&";
+                            page.thumbnail = "https://cdn.discordapp.com/icons/1257010275668852887/a_5a2bd06fd6b6b4f0a678b345f0057306.gif?size=4096";
+                            page.author.iconURL = "https://cdn.discordapp.com/icons/1257010275668852887/a_5a2bd06fd6b6b4f0a678b345f0057306.gif?size=4096";
                             break;
                         case 'basic':
-                            page.image = "https://cdn.discordapp.com/attachments/1264134884432285766/1270352605154902151/GlaceYT.gif?ex=66b3638f&is=66b2120f&hm=23878b54affd61573b105db7c1c016eb730911dc762b3fbabac5a32cbc884119&";
+                            page.image = "https://cdn.discordapp.com/icons/1257010275668852887/a_5a2bd06fd6b6b4f0a678b345f0057306.gif?size=4096";
                             page.color = "#99ccff";
-                            page.thumbnail = "https://cdn.discordapp.com/attachments/1264134884432285766/1270353776489922651/s_removed.png?ex=66b364a6&is=66b21326&hm=2d4ded259b57f476c901c58401e1d6faba2653c0a964ee1c4f371ecf3da52100&";
                             page.author.iconURL = "https://cdn.discordapp.com/attachments/1230824451990622299/1253655047259160596/6186-developer-bot.gif?ex=66a9667f&is=66a814ff&hm=1494b63ccfaf2dae30a35af520fb748dd17e76195c206f2925b526595018c60f&";
                             break;
                         case 'fun':
